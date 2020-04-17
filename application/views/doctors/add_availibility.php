b4c071e9ccbf2d159b1f65693ba550ed45a3bf9f
Priyanka Gupta <priyanka.gupta@mail.valuecoders.com>
2020-04-17 16:29:40 +0530

    docter time book

--- application/views/doctors/add_availibility.php
+++ application/views/doctors/add_availibility.php
@@ -13,18 +13,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="firstName">Date</label>
-                    <input type="text" class="form-control" id="app-date" placeholder="Select a date" required="" name="date">
+                    <input type="date" class="form-control" id="app-date" placeholder="Select a date" required="" name="date">
                 </div>
                 <div class="col-md-6 mb-3">
                     <div class="row">
                         <div class="col-md-6">
                             <label for="firstName">From time</label>
-                            <input type="text" class="form-control" placeholder="Select a time" required="" name="from_time">
+                            <select class="form-control" placeholder="Select a time" required="" name="from_time">
+                                <?php echo get_times(); ?>
+                            </select>
                         </div>
 
                         <div class="col-md-6">
                             <label for="firstName">To time</label>
-                            <input type="text" class="form-control" placeholder="Select a time" required="" name="to_time">
+                            <select class="form-control" placeholder="Select a time" required="" name="to_time">
+                                <?php echo get_times(); ?>
+                            </select>
                         </div>
                     </div>
                 </div>
@@ -36,11 +40,3 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
 </div>
 
-
-<script type="text/javascript">
-    $(function () {
-        $('#app-date').datetimepicker({
-            format: 'LT'
-        });
-    });
-</script>
